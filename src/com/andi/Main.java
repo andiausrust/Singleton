eded409eb630fd5c24c46b3781a05bf95ba2d1f8
andi <amayer80@gmail.com>
2018-09-20 07:32:10 +0200

    Initial commit

---
+++ src/com/andi/Main.java
@@ -0,0 +1,11 @@
+package com.andi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyClass myClass = MyClass.getInstance();
+
+       
+        System.out.println(myClass);
+    }
+}
---
+++ src/com/andi/MyClass.java
@@ -0,0 +1,16 @@
+package com.andi;
+
+public class MyClass {
+    private static MyClass uniqueInstance;
+
+    String name;
+
+    private MyClass() {}
+
+    public static MyClass getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new MyClass();
+        }
+        return uniqueInstance;
+    }
+}
